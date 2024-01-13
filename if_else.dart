c501db248ab5e533f0c3ec4abc3d18b61562d697
Prathamesh Mhatugade <156505272+Prathamesh5905@users.noreply.github.com>
2024-01-13 18:34:38 +0530

    Create if_else.dart

---
+++ if_else.dart
@@ -0,0 +1,18 @@
+void main() {
+  int age = 18;
+
+  if (age >= 18) {
+    print('You are an adult.');
+  } else {
+    print('You are not yet an adult.');
+  }
+
+  // Another example
+  bool isRaining = false;
+
+  if (isRaining) {
+    print('Bring an umbrella.');
+  } else {
+    print('Enjoy the weather!');
+  }
+}
