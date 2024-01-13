f3ac09a93b197d3c582ee0e32bf35268c6aeecd5
Prathamesh Mhatugade <156505272+Prathamesh5905@users.noreply.github.com>
2024-01-13 18:30:09 +0530

    Create datatype_and_variable.dart

---
+++ datatype_and_variable.dart
@@ -0,0 +1,28 @@
+void main() {
+  // Variables with different data types
+  int age = 25;
+  double height = 5.8;
+  String name = 'John';
+  bool isStudent = true;
+
+  // List
+  List<int> numbers = [1, 2, 3, 4];
+
+  // Map
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+  };
+
+  // Dynamic variable
+  dynamic dynamicVariable = 'Hello';
+
+  // Printing values
+  print('Age: $age');
+  print('Height: $height');
+  print('Name: $name');
+  print('Is a student: $isStudent');
+  print('Numbers: $numbers');
+  print('Person: $person');
+  print('Dynamic Variable: $dynamicVariable');
+}
