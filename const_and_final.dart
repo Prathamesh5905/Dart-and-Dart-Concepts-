56108ec757543198080046f2bbab99d9ed3ec4ef
Prathamesh Mhatugade <156505272+Prathamesh5905@users.noreply.github.com>
2024-01-13 18:33:18 +0530

    Create const_and_final.dart

---
+++ const_and_final.dart
@@ -0,0 +1,15 @@
+void main() {
+  // const variables
+  const int constantValue = 42;
+  const String constantString = 'Hello, const!';
+
+  // final variables
+  final int dynamicValue = 10 + 32;
+  final String finalString = 'Hello, final!';
+
+  // Printing values
+  print('Constant Value: $constantValue');
+  print('Constant String: $constantString');
+  print('Dynamic Value: $dynamicValue');
+  print('Final String: $finalString');
+}
