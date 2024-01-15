f17ac4a856f9f2e8f092f1e23bd59c78a8326345
Prathamesh Mhatugade <156505272+Prathamesh5905@users.noreply.github.com>
2024-01-15 09:38:47 +0530

    Update Fat_arrow.dart

--- Fat_arrow.dart
+++ Fat_arrow.dart
@@ -1,20 +1,5 @@
-// Short function using fat arrow
-int addNumbers(int a, int b) => a + b;
-
-// Short method using fat arrow
-class Calculator {
-  int multiply(int x, int y) => x * y;
-}
+int add(int a, int b) => a + b;
 
 void main() {
-  // Calling the function
-  int sumResult = addNumbers(5, 7);
-  print('Sum: $sumResult');
-
-  // Creating an instance of the Calculator class
-  Calculator calculator = Calculator();
-
-  // Calling the method on the instance
-  int productResult = calculator.multiply(3, 4);
-  print('Product: $productResult');
+  print(add(10, 20));
 }
